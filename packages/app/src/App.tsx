6646ed0b7c039b14a623da700ae157e12e4d065a
lderdzyan <lderdzyan@vtgsoftware.com>
2026-08-13 14:28:16 +0400

    Update Auth Code

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,34 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
+import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api'; 
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props => (
+      <SignInPage
+        {...props}
+        provider={{
+          id: 'microsoft-auth-provider',
+          title: 'Microsoft',
+          message: 'Sign in using Microsoft Entra ID',
+          apiRef: microsoftAuthApiRef,
+        }}
+      />
+    ),
+  },
+});
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [
+    catalogPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
